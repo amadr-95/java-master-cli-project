7bd7749ae7fc48022e93f42862f931ca8a7c67c6
Amador <amasabcar@outlook.com>
2023-08-23 20:51:32 +0200

    created CarDAO interface

--- src/main/java/com/project/car/CarDAO.java
+++ src/main/java/com/project/car/CarDAO.java
@@ -1,20 +1,6 @@
 package com.project.car;
 
-import java.math.BigDecimal;
+public interface CarDAO {
 
-public class CarDAO {
-
-    private static final Car[] cars;
-
-    static {
-        cars = new Car[]{
-                new Car(CarBrand.TESLA, new BigDecimal("69"), true),
-                new Car(CarBrand.AUDI, new BigDecimal("45"), false),
-                new Car(CarBrand.MERCEDES, new BigDecimal("55"), false)
-        };
-    }
-
-    public Car[] getAllCars() {
-        return cars;
-    }
+    Car[] getAllCars();
 }
