38c20280fcbc127e4f3decf178477554a020ed70
Amador <amasabcar@outlook.com>
2023-08-23 20:52:29 +0200

    created BookingDAO interface

--- src/main/java/com/project/booking/BookingDAO.java
+++ src/main/java/com/project/booking/BookingDAO.java
@@ -1,50 +1,8 @@
 package com.project.booking;
 
-import java.util.Arrays;
+public interface BookingDAO {
 
-public class BookingDAO {
-    private static final int CAPACITY = 1;
-    private static Booking[] bookings;
-    private static int nextAvailableSlot;
+    Booking[] getAllBookings();
 
-    static {
-        bookings = new Booking[CAPACITY];
-        nextAvailableSlot = 0;
-    }
-
-    public Booking[] getAllBookings() {
-        int numberOfBookings = getNumberOfBookings();
-        if (numberOfBookings == 0)
-            return new Booking[0];
-        else if (numberOfBookings == bookings.length)
-            return bookings;
-
-        Booking[] getBookings = new Booking[numberOfBookings];
-        int index = 0;
-        for (Booking booking : bookings) {
-            if (booking != null)
-                getBookings[index++] = booking;
-        }
-        return getBookings;
-    }
-
-    private int getNumberOfBookings() {
-        int count = 0;
-        for (Booking booking : bookings) {
-            if (booking != null)
-                count++;
-        }
-        return count;
-    }
-
-    public Booking saveBooking(Booking booking) {
-        if (nextAvailableSlot >= CAPACITY)
-            bookings = growBookings();
-        return bookings[nextAvailableSlot++] = booking;
-    }
-
-    private Booking[] growBookings() {
-        return Arrays.copyOf(bookings, bookings.length + 1);
-    }
-
+    Booking saveBooking(Booking booking);
 }
