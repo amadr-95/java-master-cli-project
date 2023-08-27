09eb77970dabc960d64898ff678022f38ebf786a
Amador <amasabcar@outlook.com>
2023-08-27 19:19:17 +0200

    added equals and hashcode

--- src/main/java/com/project/booking/Booking.java
+++ src/main/java/com/project/booking/Booking.java
@@ -4,6 +4,7 @@ import com.project.car.Car;
 import com.project.user.User;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.UUID;
 
 public class Booking {
@@ -33,6 +34,19 @@ public class Booking {
 
     public Car getCar() {
         return car;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Booking booking = (Booking) o;
+        return Objects.equals(uuid, booking.uuid) && Objects.equals(date, booking.date) && Objects.equals(user, booking.user) && Objects.equals(car, booking.car);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid, date, user, car);
     }
 
     @Override
